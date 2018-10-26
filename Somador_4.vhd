45467967dff57a81c432d149bd7f671df9063074
EwellyFabi <Ewelly_fab@hotmail.com>
2018-10-26 15:02:38 -0400

    Componentes

--- Somador_4.vhd
+++ Somador_4.vhd
@@ -3,10 +3,9 @@ use ieee.std_logic_1164.all;
 
 Entity Somador_4 is
 	Port(
-		A, B : IN STD_LOGIC_VECTOR(3 downto 0);
-		negate : IN STD_LOGIC;
+		A: IN STD_LOGIC_VECTOR(15 downto 0);
 		cout : OUT STD_LOGIC;
-		saida : OUT STD_LOGIC_VECTOR(3 downto 0)
+		Saida : OUT STD_LOGIC_VECTOR(15 downto 0)
 		);
 End Somador_4;
 
@@ -14,30 +13,23 @@ End Somador_4;
 Architecture somador of Somador_4 is
 BEGIN
 
-	process(A,B,negate)
-	variable temp, BorNotB: STD_LOGIC_VECTOR(3 downto 0);
-	variable C : STD_LOGIC;
+	process(A)
+
+	variable C: std_logic;
+	variable B: std_logic_vector(15 downto 0);
+	variable temp: std_logic_vector(15 downto 0);
 	
 	BEGIN 
+		C:='0';
+		B:="0000000000000100";
 	
-	-- SE  FOR SUBTRAÇÃO NEGA O B
-	if (negate = '1') then
-		BorNotB :=  NOT B;
-	else
-		BorNotB :=  B;
-	end if;
-	
-	
-	C := negate;
-	
-	-- REALIZA A SOMA OU SUBTRAÇÃO
-	for i in 0 to 3 loop
-		temp(i) := A(i) xor BorNotB(i) xor C;
-		C := (A(i) and BorNotB(i)) or ((A(i) xor BorNotB(i)) and C);
-	end loop;
-	
-	cout <= C;
-	saida <= temp;
+		for i in 0 to 15 loop
+			temp(i) := A(i) xor B(i) xor C;
+			C := (A(i) and B(i)) or ((A(i) xor B(i)) and C);
+		end loop;
+		
+		cout <= C;
+		saida <= temp;
 	
 	END process;
 	
